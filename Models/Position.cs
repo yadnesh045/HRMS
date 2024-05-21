fa7f5bd28e4d8fdd9cfbca38fac50d1121679cd2
Rahul-progamming <rahul.jadhav25052001@gmail.com>
2024-05-21 14:59:54 +0530

    Add model position

--- Models/Position.cs
+++ Models/Position.cs
@@ -2,5 +2,9 @@
 {
     public class Position
     {
+        public int Id { get; set; }
+        public string position_name { get; set; }
+        public string desciption { get; set; }
+
     }
 }
